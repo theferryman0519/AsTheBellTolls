ae6700c00c54d49e618ec0a4dd91e77f5cc3750b
The Ferryman <ferrymanstudios@gmail.com>
2026-01-16 09:19:32 -0600

    Create METHOD_TEMPLATE.cs

---
+++ Scripts/METHOD_TEMPLATE.cs
@@ -0,0 +1,87 @@
+// Main Dependencies
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+// Game Dependencies
+// using Atbt.Admin;
+// using Atbt.Animals;
+// using Atbt.Atmosphere;
+// using Atbt.Audio;
+// using Atbt.Camera;
+// using Atbt.Characters;
+// using Atbt.Cinematics;
+// using Atbt.Commissions;
+// using Atbt.Controllers;
+// using Atbt.Cooking;
+// using Atbt.Core;
+// using Atbt.Crafting;
+// using Atbt.Crops;
+// using Atbt.Data;
+// using Atbt.Dialogue;
+// using Atbt.Economy;
+// using Atbt.Events;
+// using Atbt.Foraging;
+// using Atbt.GameTime;
+// using Atbt.Input;
+// using Atbt.Inventions;
+// using Atbt.Memory;
+// using Atbt.Platform;
+// using Atbt.Player;
+// using Atbt.Player.Animation;
+// using Atbt.Player.Interaction;
+// using Atbt.Player.Inventory;
+// using Atbt.Player.Movement;
+// using Atbt.Player.Status;
+// using Atbt.Quests;
+// using Atbt.Relationships;
+// using Atbt.Restoration;
+// using Atbt.River;
+// using Atbt.Save;
+// using Atbt.Schedule;
+// using Atbt.Settings;
+// using Atbt.Shops;
+// using Atbt.TimeEffect;
+// using Atbt.TimeManipulation;
+// using Atbt.TownRanking;
+// using Atbt.Ui;
+// using Atbt.Ui.Dialogue;
+// using Atbt.Ui.Hud;
+// using Atbt.Ui.Inventory;
+// using Atbt.Ui.Notifications;
+// using Atbt.Ui.Quests;
+// using Atbt.Ui.Time;
+// using Atbt.VisualEffects;
+// using Atbt.Weather;
+// using Atbt.World;
+
+namespace TEMPLATE {
+public class METHOD_TEMPLATE : MonoBehaviour {
+
+#region -------------------- Serialized Variables --------------------
+    
+#endregion
+#region -------------------- Public Variables --------------------
+    
+#endregion
+#region -------------------- Private Variables --------------------
+    
+#endregion
+#region -------------------- Initial Functions --------------------
+    
+#endregion
+#region -------------------- Coroutines --------------------
+    
+#endregion
+#region -------------------- Public Methods --------------------
+    
+#endregion
+#region -------------------- Private Methods --------------------
+    
+#endregion
+}}
