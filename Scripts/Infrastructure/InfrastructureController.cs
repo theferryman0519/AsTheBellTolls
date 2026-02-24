134b0a45ce908af2713e5f8277964e209e9cb626
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 16:00:55 -0600

    Update InfrastructureController.cs

--- Scripts/Infrastructure/InfrastructureController.cs
+++ Scripts/Infrastructure/InfrastructureController.cs
@@ -15,10 +15,23 @@ namespace AsTheBellTolls.Infrastructure {
 public class InfrastructureController : Singleton<InfrastructureController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Sub Controllers")]
+    [SerializeField] private AdminSubController _adminSubController;
+    [SerializeField] private CoreSubController _coreSubController;
+    [SerializeField] private EventsSubController _eventsSubController;
+    [SerializeField] private GameTimeSubController _gameTimeSubController;
+    [SerializeField] private PlatformSubController _platformSubController;
+    [SerializeField] private SaveSubController _saveSubController;
+    [SerializeField] private SettingsSubController _settingsSubController;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public AdminSubController Admin => _adminSubController;
+    public CoreSubController Core => _coreSubController;
+    public EventsSubController Events => _eventsSubController;
+    public GameTimeSubController GameTime => _gameTimeSubController;
+    public PlatformSubController Platform => _platformSubController;
+    public SaveSubController Save => _saveSubController;
+    public SettingsSubController Settings => _settingsSubController;
 #endregion
 #region -------------------- Private Variables --------------------
     
