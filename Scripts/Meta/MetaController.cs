5ea4163b39d27af0307076a127425722ce9136a8
The Ferryman <ferrymanstudios@gmail.com>
2026-02-24 16:03:53 -0600

    Update MetaController.cs

--- Scripts/Meta/MetaController.cs
+++ Scripts/Meta/MetaController.cs
@@ -15,10 +15,25 @@ namespace AsTheBellTolls.Meta {
 public class MetaController : Singleton<MetaController> {
 
 #region -------------------- Serialized Variables --------------------
-    
+    [Header("Sub Controllers")]
+    [SerializeField] private CookingSubController _cookingSubController;
+    [SerializeField] private FishingSubController _fishingSubController;
+    [SerializeField] private ForagingSubController _foragingSubController;
+    [SerializeField] private InventionsSubController _inventionsSubController;
+    [SerializeField] private MemoriesSubController _memoriesSubController;
+    [SerializeField] private RelationshipsSubController _relationshipsSubController;
+    [SerializeField] private RestorationSubController _restorationSubController;
+    [SerializeField] private TonicsSubController _tonicsSubController;
 #endregion
 #region -------------------- Public Variables --------------------
-    
+    public CookingSubController Cooking => _cookingSubController;
+    public FishingSubController Fishing => _fishingSubController;
+    public ForagingSubController Foraging => _foragingSubController;
+    public InventionsSubController Inventions => _inventionsSubController;
+    public MemoriesSubController Memories => _memoriesSubController;
+    public RelationshipsSubController Relationships => _relationshipsSubController;
+    public RestorationSubController Restoration => _restorationSubController;
+    public TonicsSubController Tonics => _tonicsSubController;
 #endregion
 #region -------------------- Private Variables --------------------
     
