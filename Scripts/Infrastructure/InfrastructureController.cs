7a13607a95a3edd0c2ef0e6d4364f70099774da9
The Ferryman <ferrymanstudios@gmail.com>
2026-03-03 14:45:32 -0600

    Update InfrastructureController.cs

--- Scripts/Infrastructure/InfrastructureController.cs
+++ Scripts/Infrastructure/InfrastructureController.cs
@@ -34,7 +34,16 @@ public class InfrastructureController : Singleton<InfrastructureController> {
     public SettingsSubController Settings => _settingsSubController;
 #endregion
 #region -------------------- Private Variables --------------------
-    
+    void Start()
+    {
+	    // // FOR TESTING
+	    // PlayerPrefs.DeleteAll();
+	    // PlayerPrefs.Save();
+	    // // FOR TESTING
+	    
+        _coreSubController.IsLoaded = false;
+        SceneManager.LoadSceneAsync(_coreSubController.Scene_Main00, LoadSceneMode.Single);
+    }
 #endregion
 #region -------------------- Initial Functions --------------------
     
@@ -43,7 +52,42 @@ public class InfrastructureController : Singleton<InfrastructureController> {
     
 #endregion
 #region -------------------- Public Methods --------------------
+    public void Initialize()
+    {
+        WriteLog(this.GetType().Name, $"Initializing the controller.");
+
+        _adminSubController.Initialize();
+        _coreSubController.Initialize();
+        _eventsSubController.Initialize();
+        _gameTimeSubController.Initialize();
+        _platformSubController.Initialize();
+        _saveSubController.Initialize();
+        _settingsSubController.Initialize();
+
+        LoadingStepCompleted();
+    }
     
+    public void LoadingStepCompleted()
+	{
+		WriteLog(this.GetType().Name, $"Loading step has completed.");
+        _coreSubController.LoadingStepCompleted();
+	}
+
+    public void WriteLog(string fileName, string content)
+    {
+        _coreSubController.WriteLog(fileName, content);
+    }
+
+	public void WriteError(string fileName, string content)
+	{
+        _coreSubController.WriteError(fileName, content);
+    }
+
+	public void ChangeScene(string sceneName)
+	{
+		WriteLog(this.GetType().Name, $"Changing the scene to {sceneName}.");
+		_coreSubController.ChangeScene(sceneName);
+	}
 #endregion
 #region -------------------- Private Methods --------------------
     
